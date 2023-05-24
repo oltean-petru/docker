5d31f35c215e478dd5448584866df318bde4b3fe
Oltean Petru <poltean33@gmail.com>
2023-05-24 21:57:44 +0200

    init

---
+++ configs/nextcloud/www/nextcloud/config/config.php
@@ -0,0 +1,16 @@
+<?php
+$CONFIG = array (
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'datadirectory' => '/data',
+  'instanceid' => 'oc699ymdhrzl',
+  'passwordsalt' => 'RD/RACI357pUKq0mjDg3e0fxUVa05y',
+  'secret' => '6+ClGyc3mB1I0xDib/jbe4iEPoeQ8sMYF4f6ziORNWzzEYAA',
+  'trusted_domains' => 
+  array (
+    0 => 'nextcloud.ynovportal.duckdns.org',
+  ),
+  'dbtype' => 'sqlite3',
+  'version' => '26.0.1.1',
+  'overwrite.cli.url' => 'https://nextcloud.ynovportal.duckdns.org',
+  'installed' => true,
+);
